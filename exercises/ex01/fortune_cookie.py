102fcb938598d1eb16b30770b8a237e0e1497b5a
Joe Coggin <cogginj@live.unc.edu>
2021-02-05 21:08:32 -0500

    Progress on Exercise 1

--- exercises/ex01/fortune_cookie.py
+++ exercises/ex01/fortune_cookie.py
@@ -1,6 +1,6 @@
 """Program that outputs one of at least four random, good fortunes."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730237793"
 
 # The randint function is imported from the random library so that
 # you are able to generate integers at random.
@@ -13,3 +13,22 @@ from random import randint
 
 
 # Begin your solution here...
+print("Your fortune cookie says...")
+
+message = randint(1,4)
+
+if message == 1:
+    print("A journey of a thousand miles begins with a single step")
+else:
+    if message == 2:
+        print("Fortune favors the brave")
+    if message == 3:
+        print("Help, I'm stuck in a fortune cookie factory")
+    if message == 4:
+        print("Stop wishing. Start doing")
+
+
+
+
+
+print("Now, go spread positive vibes")
--- exercises/ex01/vaccine_calc.py
+++ exercises/ex01/vaccine_calc.py
@@ -1,6 +1,6 @@
 """A vaccination calculator."""
 
-__author__ = "YOUR PID HERE"
+__author__ = "730237793"
 
 # The datetime data type is imported from the datetime library.
 # A datetime object models a specific date and time.
@@ -18,3 +18,26 @@ from datetime import timedelta
 
 
 # Begin your solution here...
+Population: int = int(input("Population: "))
+Doses_administered: int = int(input("Doses administered: "))
+Doses_per_day: int = int(input("Doses per day: "))
+Target_percent_vaccinated: int = int(input("Target percent vaccinated: "))
+
+Additional_vaccines: float = ((2 * Population * (Target_percent_vaccinated/100)) - Doses_administered) 
+Days_until_target_percent_vaccinated: float = Additional_vaccines / Doses_per_day
+round(Days_until_target_percent_vaccinated)
+
+Days: timedelta = timedelta(Days_until_target_percent_vaccinated)
+today: datetime = datetime.today()
+Vaccination_date: datetime = today + Days
+Date: str = Vaccination_date.strftime("%B %d, %Y")
+
+string = ("We will be {}% vaccinated in {} days which will fall on {}") .format(Target_percent_vaccinated, Days_until_target_percent_vaccinated, Date)
+
+print(string)
+
+
+
+
+
+
